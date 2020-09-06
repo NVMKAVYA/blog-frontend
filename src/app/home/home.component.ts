95c5ff43b00f41294f48ba8396b06f87a6e8c74f
NVM KAVYA <meherkavya@gmail.com>
2020-09-06 18:51:09 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+// decorator is enabling the below class to become part of Angular framework
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
